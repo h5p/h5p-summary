b14d74e92380c23d899fa62091b19ce4a684a05d
Preben Borch <preben@localhost.localdomain>
2013-02-28 12:14:35 +0100

    Under construction

--- js/summary.js
+++ js/summary.js
@@ -23,21 +23,21 @@ H5P.Summary = function (options, contentId) {
 		$myDom = $target;
 		$myDom.css({ border: '1px solid red', padding: '10px', backgroundColor: 'lightgrey'});
 
-	var elements = Array();
+		var elements = Array();
 
-	// Create array objects
-	var c=0;
-	for (var i = 0; i < options.summaries.length; i++) {
-		elements[i] = Array();
-		for (var j = 0; j < options.summaries[i].length; j++) {
-			answer[c] = j == 0;
-			elements[i][j] = {
-				id: c++,
-				node: j,
-				text: options.summaries[i][j],
-				correct: j == 0 // First summary is correct
-			};
-		}
+		// Create array objects
+		var c=0;
+		for (var i = 0; i < options.summaries.length; i++) {
+			elements[i] = Array();
+			for (var j = 0; j < options.summaries[i].length; j++) {
+				answer[c] = j == 0;
+				elements[i][j] = {
+					id: c++,
+					node: j,
+					text: options.summaries[i][j],
+					correct: j == 0 // First summary is correct
+				};
+			}
 
 		// Randomize elements
 		for (var k = elements[i].length - 1; k > 0; k--) {
@@ -48,11 +48,13 @@ H5P.Summary = function (options, contentId) {
 		}
 	}
 
+	// Create content
 	var $summary = $('<div class="summary" id="summary-list" xpanel="'+i+'" style="background-color: lightgray; height: 200px">');
 	var $evaluation = $('<div class="evaluation" id="option-list" style="">Velg riktig alternativ til å legge til oppsummeringen</div>');
 	var $score = $('<div class="score" id="score" style="float: right"></div>');
-	var $options = $('<div class="options" id="option-list" xpanel="'+i+'" style="padding: 10px; overflow: hidden; background-color: white; height: 100px">');
+	var $options = $('<div class="options" id="option-list" xpanel="'+i+'" style="padding: 10px; overflow: hidden; background-color: white; height: 150px">');
 
+	// Insert content
 	$myDom.append($summary);
 	$evaluation.append($score);
 	$myDom.append($evaluation);
@@ -60,7 +62,7 @@ H5P.Summary = function (options, contentId) {
 
 	// Add elements to content
 	for (var i = 0; i < elements.length; i++) {
-		var $page = $('<div class="summary-entries" id="panel-'+i+'" xpanel="'+i+'" style="border: 1px solid #CCCCCC; display: none">');
+		var $page = $('<div class="summary-entries" id="panel-'+i+'" xpanel="'+i+'" style="display: none">');
 
 		for (var j = 0; j < elements[i].length; j++) {
 			var $node = $('<div id="node-'+elements[i][j].id+'" class="summary-entry" node="'+elements[i][j].id+'">'+elements[i][j].text+'</div>');
@@ -77,7 +79,6 @@ H5P.Summary = function (options, contentId) {
 
 				// Correct answer?
 				if(answer[node_id]){
-
 					var $answer = $('<div class="" id="" style="">'+$el.html()+'</div>');
 					$summary.append($answer);
 
@@ -85,18 +86,21 @@ H5P.Summary = function (options, contentId) {
 					var $curr_panel = $('#panel-'+panel, $myDom);
 					var $next_panel = $('#panel-'+(panel + 1), $myDom);
 
-					console.log(node_id+": Clicked answer " + answer[node_id] + ' in panel '+$el.attr('panel'));
-
-					// Hide current
+					// Hide this question set panel
 					$curr_panel.slideUp('slow', function() {
-						// $curr_panel.css({ display: 'none' });
 						console.log("Fade out done " + this);
 					});
 
 					// Show next if present
 					if($next_panel.attr('id')){
 						$next_panel.css({ display: 'block' });
-						console.log("p="+$next_panel.attr('id'));
+					}
+					else {
+						// Show final evaluation
+						var $score = $('#score', $myDom);
+						var $evaluation = $('<div class="" id="" style="">OK. Du hadde '+score+' feil</div>');
+						$summary.append($evaluation);
+						$score.html('');
 					}
 				}
 				else {
@@ -113,9 +117,9 @@ H5P.Summary = function (options, contentId) {
 		$options.append($page);
 	}
 
-	var $c = $('#panel-0', $myDom);
-	$c.css({ border: '1px solid green', display: 'block' });
-    return this;
+		var $c = $('#panel-0', $myDom);
+		$c.css({ display: 'block' });
+		return this;
   };
 
   // Masquerade the main object to hide inner properties and functions.
