31619b49c9a124a65d203315367b206d351e406f
Pål Jørgensen <pal.jorgensen@amendor.com>
2014-08-05 16:57:16 +0200

    Updated upgrades.js to improved format

--- upgrades.js
+++ upgrades.js
@@ -3,16 +3,17 @@ var H5PUpgrades = H5PUpgrades || {};
 H5PUpgrades['H5P.Summary'] = (function ($) {
   return {
     1: {
-      1: function (parameters, finished) {
-        
-        // Wrap summaries to allow tip.
-        for (var i = 0; i< parameters.summaries.length; i++) {
-          parameters.summaries[i] = {
-            summary: parameters.summaries[i]
-          };
+      1: {
+        contentUpgrade: function (parameters, finished) {
+          // Wrap summaries to allow tip.
+          for (var i = 0; i < parameters.summaries.length; i++) {
+            parameters.summaries[i] = {
+              summary: parameters.summaries[i]
+            };
+          }
+          
+          finished(null, parameters);
         }
-        
-        finished(null, parameters);
       }
     }
   };
