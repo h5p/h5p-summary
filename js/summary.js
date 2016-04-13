7e83c1a464235bf07e3169816ab2612c38456087
steima <matthias@matthias.mbp.local>
2016-04-13 15:59:40 -0400

    tabindex=-1 on radios

--- js/summary.js
+++ js/summary.js
@@ -340,7 +340,7 @@ H5P.Summary = (function ($, Question) {
 
 	var item_id = "summary_" + j + "_" + element.summaries[j].id;
         var $node = $('<div data-bit="' + element.summaries[j].id + '" class="' + summaryLineClass + '">' +
-	  '<input type="radio" id="' + item_id + '" class="sr-only" value="' + element.summaries[j].id + '">' +
+	  '<input type="radio" id="' + item_id + '" class="sr-only" value="' + element.summaries[j].id + '" tabindex="-1">' +
 	  '<label for="' + item_id + '" tabindex="0">' +
             element.summaries[j].text +
           '</label>' +
